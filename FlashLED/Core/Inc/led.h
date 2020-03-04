10a8232bcd8772c3840b1c38b57d665d2d26e861
BusKetZz <busketz2k@gmail.com>
2020-03-04 11:34:55 +0100

    FlashLED/Core/Inc/led.h: change from HAL to LL

--- FlashLED/Core/Inc/led.h
+++ FlashLED/Core/Inc/led.h
@@ -1,17 +1,3 @@
-#include "stm32f4xx_hal.h"
-#include "stm32f4xx_hal_gpio.h"
-
-
-
-/*****************************************************************************/
-/*                             PUBLIC DEFINES                                */
-/*****************************************************************************/
-
-#define LED2_ON()  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET)
-#define LED2_OFF() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET)
-
-
-
 /*****************************************************************************/
 /*                       FreeRTOS TASK DECLARATION                           */
 /*****************************************************************************/
