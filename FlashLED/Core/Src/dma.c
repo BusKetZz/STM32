da9d2c0e375ef6bae9d3299c161fed8f0882fd3c
BusKetZz <busketz2k@gmail.com>
2020-03-05 09:16:22 +0100

    FlashLED/Core/Inc,Src/dma.h,c: configuration for USART_Tx channel removed

--- FlashLED/Core/Src/dma.c
+++ FlashLED/Core/Src/dma.c
@@ -16,14 +16,11 @@ void DMA2_Clock_Config(void)
 
 
 
-void DMA2_NVIC_Config(void)
+void DMA2_USART1_RX_NVIC_Config(void)
 {
   NVIC_SetPriority(DMA2_Stream2_IRQn, 
                    NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
   NVIC_EnableIRQ(DMA2_Stream2_IRQn);
-  NVIC_SetPriority(DMA2_Stream7_IRQn, 
-                   NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
-  NVIC_EnableIRQ(DMA2_Stream7_IRQn);
 }
 
 
@@ -40,20 +37,4 @@ void DMA2_USART1_RX_Config(void)
   LL_DMA_SetPeriphSize(DMA2, LL_DMA_STREAM_2, LL_DMA_PDATAALIGN_BYTE);
   LL_DMA_SetMemorySize(DMA2, LL_DMA_STREAM_2, LL_DMA_MDATAALIGN_BYTE);
   LL_DMA_DisableFifoMode(DMA2, LL_DMA_STREAM_2);
-}
-
-
-
-void DMA2_USART1_TX_Config(void)
-{
-  LL_DMA_SetChannelSelection(DMA2, LL_DMA_STREAM_7, LL_DMA_CHANNEL_4);
-  LL_DMA_SetDataTransferDirection(DMA2, LL_DMA_STREAM_7, 
-                                  LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
-  LL_DMA_SetStreamPriorityLevel(DMA2, LL_DMA_STREAM_7, LL_DMA_PRIORITY_LOW);
-  LL_DMA_SetMode(DMA2, LL_DMA_STREAM_7, LL_DMA_MODE_NORMAL);
-  LL_DMA_SetPeriphIncMode(DMA2, LL_DMA_STREAM_7, LL_DMA_PERIPH_NOINCREMENT);
-  LL_DMA_SetMemoryIncMode(DMA2, LL_DMA_STREAM_7, LL_DMA_MEMORY_INCREMENT);
-  LL_DMA_SetPeriphSize(DMA2, LL_DMA_STREAM_7, LL_DMA_PDATAALIGN_BYTE);
-  LL_DMA_SetMemorySize(DMA2, LL_DMA_STREAM_7, LL_DMA_MDATAALIGN_BYTE);
-  LL_DMA_DisableFifoMode(DMA2, LL_DMA_STREAM_7);
 }
