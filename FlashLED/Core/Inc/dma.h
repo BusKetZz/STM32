23e00621a198d206254213d5b9f34fed35fe7fde
BusKetZz <busketz2k@gmail.com>
2020-03-05 14:06:29 +0100

    FlashLED/Core/Inc,Src/dma.h,c: DMA2 for USART1_RX ready for debug

--- FlashLED/Core/Inc/dma.h
+++ FlashLED/Core/Inc/dma.h
@@ -3,5 +3,5 @@
 /*****************************************************************************/
 
 void DMA2_Clock_Config(void);
-void DMA2_USART1_RX_NVIC_Config(void);
 void DMA2_USART1_RX_Config(void);
+void DMA2_USART1_RX_NVIC_Config(void);
