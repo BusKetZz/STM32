297da6c489806684d519a0c4805fed274a0c2d01
BusKetZz <busketz2k@gmail.com>
2020-03-11 08:08:31 +0100

    STM32F401RE-Nucleo/Projects/FlashLED/Components/Inc,Src/: idle_task.h,c added

---
+++ STM32F401RE-Nucleo/Projects/FlashLED/Components/Inc/idle_task.h
@@ -0,0 +1,23 @@
+#ifndef IDLE_TASK_H
+#define IDLE_TASK_H
+
+#ifdef  __cplusplus
+extern "C"
+{
+#endif
+
+
+
+/*****************************************************************************/
+/*                          RTOS TASK DECLARATION                            */
+/*****************************************************************************/
+
+void StartIdleTask(void *argument);
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+ 
+#endif  /* IDLE_TASK_H */
