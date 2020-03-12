5ba83aedfdfd3266adad52799aba027dc646dd32
BusKetZz <busketz2k@gmail.com>
2020-03-12 13:42:49 +0100

    STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/dma.c: initial data processing in Dma2Usart1RxTask added (debug build succes)

--- STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/dma.c
+++ STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/dma.c
@@ -6,15 +6,20 @@
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_dma.h"
 
+#include <string.h>
+
 
 
 /*****************************************************************************/
 /*                            PRIVATE DEFINES                                */
 /*****************************************************************************/
 
-#define BUFFER_SIZE 32
+#define DMA2_BUFFER_SIZE 32
 
 #define LED2TASK_QUEUE_MESSAGES_COUNT (uint32_t)1
+
+#define MAGIC_FOUND 0
+#define MAGIC_NOT_FOUND -1
 
 
 
@@ -30,11 +35,19 @@
 /*                           PRIVATE VARIABLES                               */
 /*****************************************************************************/
 
-static uint8_t dma2Usart1RxBuffer[BUFFER_SIZE];
+static uint8_t dma2Usart1RxBuffer[DMA2_BUFFER_SIZE];
 
 static const char *MAGIC_WORD = "ABCD";
 
 static osMessageQueueId_t queueHandleForLed2Task;
+
+
+
+/*****************************************************************************/
+/*                     PRIVATE FUNCTIONS DECLARATIONS                        */
+/*****************************************************************************/
+
+static size_t GetCurrentPositionInDma2Usart1RxBuffer(void);
 
 
 
@@ -66,18 +79,7 @@ void DMA2_USART1_RX_Config(void)
   LL_DMA_SetMemoryAddress(DMA2, LL_DMA_STREAM_2, (uint32_t)dma2Usart1RxBuffer);
   LL_DMA_SetDataLength(DMA2, LL_DMA_STREAM_2, ARRAY_LENGTH(dma2Usart1RxBuffer));
 
-  LL_DMA_EnableIT_TC(DMA2, LL_DMA_STREAM_2);
-
   LL_DMA_EnableStream(DMA2, LL_DMA_STREAM_2);
-}
-
-
-
-void DMA2_USART1_RX_NVIC_Config(void)
-{
-  NVIC_SetPriority(DMA2_Stream2_IRQn, 
-                   NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
-  NVIC_EnableIRQ(DMA2_Stream2_IRQn);
 }
 
 
@@ -95,14 +97,60 @@ osMessageQueueId_t GetQueueHandleForLed2Task(void)
 
 void StartDma2Usart1RxTask(void *argument)
 {
+  size_t oldPosition = 0;
+  size_t currentPosition = 0;
+  size_t magicWordLength = strlen(MAGIC_WORD);
+  int isMagicFound = MAGIC_NOT_FOUND;
   uint8_t led2UpdatedBlinksCount[2] = {0};
+
   queueHandleForLed2Task = osMessageQueueNew(LED2TASK_QUEUE_MESSAGES_COUNT, 
                                       sizeof(led2UpdatedBlinksCount), NULL);
 
   for(;;)
   {
-    /* TODO */
+    /* Copy data from DMA buffer into temporary processing buffer.
+       If in temporary buffer is more than 6 bytes check whether is there a 
+       Magic Word. If not wait for next data and check again but start
+       from the next position. If Magic Word is found, update values of blinks
+       count in additional buffer.
+    */
+    currentPosition = GetCurrentPositionInDma2Usart1RxBuffer();
+    if(currentPosition != oldPosition)
+    {
+      while((currentPosition - oldPosition) >= magicWordLength)
+      {
+        isMagicFound = memcmp(dma2Usart1RxBuffer + oldPosition, MAGIC_WORD,
+                              magicWordLength);
+        if(isMagicFound == MAGIC_FOUND)
+        {
+          oldPosition += magicWordLength;
+          memcpy(led2UpdatedBlinksCount, dma2Usart1RxBuffer + oldPosition, 2);
+          oldPosition = currentPosition;
+          break;
+        }
 
-    osMessageQueuePut(queueHandleForLed2Task, led2UpdatedBlinksCount, 1, 0);
+        oldPosition++;
+      }
+
+    }
+
+
+    if(isMagicFound == MAGIC_FOUND)
+    {
+      osMessageQueuePut(queueHandleForLed2Task, led2UpdatedBlinksCount, 0, 0);
+      isMagicFound = MAGIC_NOT_FOUND;
+    }
   }
+}
+
+
+
+/*****************************************************************************/
+/*                     PRIVATE FUNCTIONS DEFINITIONS                         */
+/*****************************************************************************/
+
+static size_t GetCurrentPositionInDma2Usart1RxBuffer(void)
+{
+  return ARRAY_LENGTH(dma2Usart1RxBuffer) - 
+         LL_DMA_GetDataLength(DMA2, LL_DMA_STREAM_2);
 }
