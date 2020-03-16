0e71444640cd2ffee60054a6344fc079c03f963c
BusKetZz <busketz2k@gmail.com>
2020-03-16 13:02:11 +0100

    FlashLED/: project directory removed

--- FlashLED/Core/Inc/dma.h
+++
@@ -1,7 +0,0 @@
-/*****************************************************************************/
-/*                    PUBLIC FUNCTIONS DECLARATIONS                          */
-/*****************************************************************************/
-
-void DMA2_Clock_Config(void);
-void DMA2_USART1_RX_Config(void);
-void DMA2_USART1_RX_NVIC_Config(void);
--- FlashLED/Core/Inc/gpio.h
+++
@@ -1,7 +0,0 @@
-/*****************************************************************************/
-/*                    PUBLIC FUNCTIONS DECLARATIONS                          */
-/*****************************************************************************/
-
-void GPIOA_Clock_Config(void);
-void GPIOA_LED2_Config(void);
-void GPIOA_USART1_RX_Config(void);
