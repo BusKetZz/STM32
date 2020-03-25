c10bbdb2424f8b88432d632c7c7f25ccb8151cff
BusKetZz <busketz2k@gmail.com>
2020-03-25 15:33:26 +0100

    STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/adc_temperature_regulator.c: ADC1_TEMPERATURE_REGULATOR_Settings_Config() procedure definition added

--- STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/adc_temperature_regulator.c
+++ STM32F401RE-Nucleo/Projects/FlashLED/Components/Src/adc_temperature_regulator.c
@@ -13,3 +13,39 @@ void ADC1_TEMPERATURE_REGULATOR_Clock_Config(void)
 {
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_ADC1);
 }
+
+
+
+void ADC1_TEMPERATURE_REGULATOR_Settings_Config(void)
+{
+  LL_ADC_InitTypeDef ADC1_TEMPERATURE_REGULATOR_InitStruct =
+  {
+    .Resolution         = LL_ADC_RESOLUTION_12B,
+    .DataAlignment      = LL_ADC_DATA_ALIGN_RIGHT,
+    .SequencersScanMode = LL_ADC_SEQ_SCAN_DISABLE
+  };
+  LL_ADC_Init(ADC1, &ADC1_TEMPERATURE_REGULATOR_InitStruct);
+  LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_0,
+                                LL_ADC_SAMPLINGTIME_480CYCLES);
+
+  LL_ADC_REG_InitTypeDef ADC1_TEMPERATURE_REGULATOR_REG_InitStruct =
+  {
+    .TriggerSource    = LL_ADC_REG_TRIG_SOFTWARE,
+    .SequencerLength  = LL_ADC_REG_SEQ_SCAN_DISABLE,
+    .SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE,
+    .ContinuousMode   = LL_ADC_REG_CONV_SINGLE,
+    .DMATransfer      = LL_ADC_REG_DMA_TRANSFER_NONE 
+  };
+  LL_ADC_REG_Init(ADC1, &ADC1_TEMPERATURE_REGULATOR_REG_InitStruct);
+  LL_ADC_REG_SetFlagEndOfConversion(ADC1, LL_ADC_REG_FLAG_EOC_UNITARY_CONV);
+  LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_0);
+
+  LL_ADC_CommonInitTypeDef ADC1_TEMPERATURE_REGULATOR_CommonInitStruct =
+  {
+    .CommonClock = LL_ADC_CLOCK_SYNC_PCLK_DIV8
+  };
+  LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC1),
+                    &ADC1_TEMPERATURE_REGULATOR_CommonInitStruct);
+
+  LL_ADC_DisableIT_EOCS(ADC1);
+}
