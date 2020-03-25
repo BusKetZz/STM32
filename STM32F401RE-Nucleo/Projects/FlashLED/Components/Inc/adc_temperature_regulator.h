7c31743a6a2d0ce4d8ad10b46d6ce63448af8fd7
BusKetZz <busketz2k@gmail.com>
2020-03-25 14:15:47 +0100

    STM32F401RE-Nucleo/Projects/FlashLED/Components/Inc/adc_temperature_regulator.h: ADC1_TEMPERATURE_REGULATOR_Clock_Config() procedure declaration added

---
+++ STM32F401RE-Nucleo/Projects/FlashLED/Components/Inc/adc_temperature_regulator.h
@@ -0,0 +1,28 @@
+#ifndef ADC_TEMPERATURE_REGULATOR_H
+#define ADC_TEMPERATURE_REGULATOR_H
+
+#ifdef  __cplusplus
+extern "C"
+{
+#endif
+
+
+
+/*****************************************************************************/
+/*                    PUBLIC FUNCTIONS DECLARATIONS                          */
+/*****************************************************************************/
+
+void ADC1_TEMPERATURE_REGULATOR_Clock_Config(void);
+
+
+
+
+
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  /* ADC_TEMPERATURE_REGULATOR_H */
