4471071b1669bd8127414179bd98f08b3668cf86
BusKetZz <busketz2k@gmail.com>
2020-05-09 23:59:00 +0200

    STM32F401RE-Nucleo/Projects/bare_metal_tutorial/source/main.c: testing semihosting printf() function

--- STM32F401RE-Nucleo/Projects/bare_metal_tutorial/source/main.c
+++ STM32F401RE-Nucleo/Projects/bare_metal_tutorial/source/main.c
@@ -1,9 +1,21 @@
 #include <stm32f401xe.h>
 
+#include <stdio.h>
+
+
+
+extern void initialise_monitor_handles(void);
+
 
 
 int main()
 {
+	initialise_monitor_handles();
 
+	printf("Hello World!\n");
+
+	while(1) {
+		;
+	}
 }
 
