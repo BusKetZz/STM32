3b78134f846fac150240ef7ba6ee430fc30d640d
BusKetZz <busketz2k@gmail.com>
2020-05-21 22:12:55 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c: gpio_clock_enable API definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c
@@ -7,3 +7,37 @@
 
 #include "stm32f401_driver_gpio.h"
 
+#include "stm32f401xe.h"
+
+
+
+/*****************************************************************************/
+/* GPIO API definitions */
+/*****************************************************************************/
+
+void gpio_clock_enable(gpio_registers_t *gpio_port)
+{
+    switch(gpio_port) {
+        case GPIOA:
+            GPIOA_CLOCK_ENABLE();
+            break;
+        case GPIOB:
+            GPIOB_CLOCK_ENABLE();
+            break;
+        case GPIOC:
+            GPIOC_CLOCK_ENABLE();
+            break;
+        case GPIOD:
+            GPIOD_CLOCK_ENABLE();
+            break;
+        case GPIOE:
+            GPIOE_CLOCK_ENABLE();
+            break;
+        case GPIOH:
+            GPIOH_CLOCK_ENABLE();
+            break;
+        default:
+            /* TODO */
+    }
+}
+
