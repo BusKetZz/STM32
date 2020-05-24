500b6ffebee2a250730408f0ad8e374153661dca
BusKetZz <busketz2k@gmail.com>
2020-05-24 22:15:05 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_gpio.h: gpio_output_speed enum changed to typedef enum gpio_output_speed_t

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_gpio.h
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_gpio.h
@@ -34,12 +34,12 @@ typedef enum gpio_output_type {
 
 
 
-enum gpio_output_speed {
+typedef enum gpio_output_speed {
     gpio_output_speed_low = 0,
     gpio_output_speed_medium,
     gpio_output_speed_high,
     gpio_output_speed_very_high
-};
+}gpio_output_speed_t;
 
 
 
