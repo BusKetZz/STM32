2d4355f4885a03541a54b54589bb785d6664fc3a
BusKetZz <busketz2k@gmail.com>
2020-05-31 21:53:35 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/source/nvic_irq.c: nvic_irq_enable() function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/source/nvic_irq.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/source/nvic_irq.c
@@ -9,3 +9,20 @@
 
 #include "nvic_irq.h"
 
+
+
+/*****************************************************************************/
+/* NVIC IRQ API DEFINITIONS */
+/*****************************************************************************/
+
+void nvic_irq_enable(nvic_irq_number_t irq_number)
+{
+    if(irq_number <= 31) {
+        *NVIC_ISER0_BASE_ADDRESS |= (1 << irq_number);
+    } else if(irq_number > 31 && irq_number <= 63) {
+        *NVIC_ISER1_BASE_ADDRESS |= (1 << (irq_number % 32));
+    } else if(irq_number > 63 && irq_number <= 95) {
+        *NVIC_ISER2_BASE_ADDRESS |= (1 << (irq_number % 64));
+    }
+}
+
