055996c3d3215005f3a30f57c152d0ccacbb2569
BusKetZz <busketz2k@gmail.com>
2020-05-31 21:57:53 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c: gpio_pin_irq_disable() function definition deleted

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_gpio.c
@@ -267,19 +267,6 @@ void gpio_pin_irq_config(gpio_handle_t *gpio_handle)
 
 
 
-void gpio_pin_irq_disable(nvic_irq_number_t irq_number)
-{
-    if(irq_number <= 31) {
-        *NVIC_ICER0_BASE_ADDRESS |= (1 << irq_number);
-    } else if(irq_number > 31 && irq_number <= 63) {
-        *NVIC_ICER1_BASE_ADDRESS |= (1 << (irq_number % 32));
-    } else if(irq_number > 63 && irq_number <= 95) {
-        *NVIC_ICER2_BASE_ADDRESS |= (1 << (irq_number % 64));
-    } 
-}
-
-
-
 /*****************************************************************************/
 /* GPIO helper functions definitions */
 /*****************************************************************************/
