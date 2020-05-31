6b08956d824691a30d6db560ff8d252c3532b7eb
BusKetZz <busketz2k@gmail.com>
2020-05-31 16:27:53 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/cortex_m4.h: base addresses of NVIC_ISER0 to NVIC_ISER3 registers added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/cortex_m4.h
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/cortex_m4.h
@@ -10,6 +10,15 @@
 
 
 
+/*****************************************************************************/
+/* NVIC REGISTERS BASE ADDRESSES */
+/*****************************************************************************/
+
+#define NVIC_ISER0_BASE_ADDRESS (volatile uint32_t *)0xE000E100
+#define NVIC_ISER1_BASE_ADDRESS (volatile uint32_t *)0xE000E104
+#define NVIC_ISER2_BASE_ADDRESS (volatile uint32_t *)0xE000E108
+#define NVIC_ISER3_BASE_ADDRESS (volatile uint32_t *)0xE000E10C
+
 
 
 #endif /* CORTEX_M4_H */ 
