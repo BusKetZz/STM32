348bc3b33f91a864d4a7855cf9d0aedb7cc03734
BusKetZz <busketz2k@gmail.com>
2020-06-02 22:58:51 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/: stm32f401xe_driver_spi.h header file initialized with basic information and header guard added

---
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_spi.h
@@ -0,0 +1,18 @@
+/*
+ * Project: Drivers development tutorial
+ * Target MCU: STM32F401XE
+ * Author: Jakub Standarski 
+ *
+ */
+
+#ifndef STM32F401XE_DRIVER_SPI_H
+#define STM32F401XE_DRIVER_SPI_H
+
+
+
+
+
+
+
+#endif /* STM32F401XE_DRIVER_SPI_H */
+
