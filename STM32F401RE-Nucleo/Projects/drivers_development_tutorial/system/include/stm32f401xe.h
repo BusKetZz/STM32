19118f9960ec290b92c45f14666695710ef03b1f
BusKetZz <busketz2k@gmail.com>
2020-06-03 13:23:57 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/stm32f401xe.h: spi_registers_t structure added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/stm32f401xe.h
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/stm32f401xe.h
@@ -312,5 +312,21 @@ typedef struct {
 
 
 
+/* SPI REGISTERS STRUCTURE */
+
+typedef struct {
+    volatile uint32_t CR1;
+    volatile uint32_t CR2;
+    volatile uint32_t SR;
+    volatile uint32_t DR;
+    volatile uint32_t CRCPR;
+    volatile uint32_t RXCRCR;
+    volatile uint32_t TXCRCR;
+    volatile uint32_t I2SCFGR;
+    volatile uint32_t I2SPR;
+}spi_registers_t;
+
+
+
 #endif /* STM32F401XE_H */
 
