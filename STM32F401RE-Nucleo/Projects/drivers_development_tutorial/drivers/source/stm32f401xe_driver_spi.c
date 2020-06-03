1d068af5134079c3dfbaa8759d70d5eaeacf7b71
BusKetZz <busketz2k@gmail.com>
2020-06-03 14:20:20 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c: SPI clock enable and disable macros added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c
@@ -9,3 +9,21 @@
 
 #include "stm32f401xe.h"
 
+
+
+/*****************************************************************************/
+/* SPI CLOCK ENABLE AND DISABLE MACROS */
+/*****************************************************************************/
+
+#define SPI1_CLOCK_ENABLE() ( RCC->APB2ENR |= (1 << 12) )
+#define SPI2_CLOCK_ENABLE() ( RCC->APB1ENR |= (1 << 14) )
+#define SPI3_CLOCK_ENABLE() ( RCC->APB1ENR |= (1 << 15) )
+#define SPI4_CLOCK_ENABLE() ( RCC->APB2ENR |= (1 << 13) )
+
+
+
+#define SPI1_CLOCK_DISABLE() ( RCC->APB2ENR &= ~(1 << 12) )
+#define SPI2_CLOCK_DISABLE() ( RCC->APB1ENR &= ~(1 << 14) )
+#define SPI3_CLOCK_DISABLE() ( RCC->APB1ENR &= ~(1 << 15) )
+#define SPI4_CLOCK_DISABLE() ( RCC->APB2ENR &= ~(1 << 13) )
+
