e4b3ba3dad68884e36dd69b0ab1c1ade224355fc
BusKetZz <busketz2k@gmail.com>
2020-06-04 15:59:23 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c: spi_init_config() function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_spi.c
@@ -61,3 +61,57 @@ void spi_clock_disable(spi_registers_t *spi_port)
     } 
 }
 
+
+
+void spi_init_config(spi_handle_t *spi_handle)
+{
+    uint32_t register_settings = 0;
+
+    register_settings |= (spi_handle->spi_config.device_mode << 2);
+
+    if(spi_handle->spi_config.transfer_mode == spi_transfer_mode_full_duplex) {
+        register_settings &= ~(1 << 15);
+    } else if(spi_handle->spi_config.transfer_mode ==
+        spi_transfer_mode_half_duplex) {
+        register_settings |= (1 << 15);
+    } else if(spi_handle->spi_config.transfer_mode ==
+        spi_transfer_mode_simplex_rxonly) {
+        register_settings &= ~(1 << 15);
+        register_settings |= (1 << 10);
+    }
+
+    register_settings |= (spi_handle->spi_config.baudrate << 3);
+
+    if(spi_handle->spi_config.clock_polarity ==
+        spi_clock_polarity_low_idle_state) {
+        register_settings &= ~(1 << 1);
+    } else if(spi_handle->spi_config.clock_polarity ==
+        spi_clock_polarity_high_idle_state) {
+        register_settings |= (1 << 1);
+    }
+
+    if(spi_handle->spi_config.clock_phase == spi_clock_phase_first) {
+        register_settings &= ~(1 << 0);
+    } else if(spi_handle->spi_config.clock_phase == spi_clock_phase_second) {
+        register_settings |= (1 << 0);
+    }
+
+    if(spi_handle->spi_config.data_frame_format ==
+        spi_data_frame_format_8bits) {
+        register_settings &= ~(1 << 11);
+    } else if(spi_handle->spi_config.data_frame_format ==
+        spi_data_frame_format_16bits) {
+        register_settings |= (1 << 11);
+    }
+
+    if(spi_handle->spi_config.software_slave_management ==
+        spi_software_slave_management_disabled) {
+        register_settings &= ~(1 << 9);
+    } else if(spi_handle->spi_config.software_slave_management ==
+        spi_software_slave_management_enabled) {
+        register_settings |= (1 << 9);
+    }
+
+    spi_handle->spi_port->CR1 = register_settings;
+}
+
