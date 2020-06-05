376333e8b3f10caa7ce31c1d4abdb96d08f93375
BusKetZz <busketz2k@gmail.com>
2020-06-05 13:11:35 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_spi.h: spi_read_data() function prototype added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_spi.h
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_spi.h
@@ -110,6 +110,8 @@ void spi_disable(spi_registers_t *spi_port);
 
 void spi_send_data(spi_registers_t *spi_port, uint8_t *tx_buffer,
     uint32_t bytes_to_transmit);
+void spi_read_data(spi_registers_t *spi_port, uint8_t *rx_buffer,
+    uint32_t bytes_to_read);
 
 
 
