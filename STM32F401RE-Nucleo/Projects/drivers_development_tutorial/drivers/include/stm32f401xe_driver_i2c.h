2cb882db31126d2c6fa306b7368ef3e9f55f010d
BusKetZz <busketz2k@gmail.com>
2020-06-07 18:08:21 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/: stm32f401xe_driver_i2c.h empty file with header and header guard added

---
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_i2c.h
@@ -0,0 +1,17 @@
+/*
+ * Project: Drivers development tutorial
+ * Target MCU: STM32F401XE
+ * Author: Jakub Standarski 
+ *
+ */
+
+#ifndef STM32F401XE_DRIVER_I2C_H
+#define STM32F401XE_DRIVER_I2C_H
+
+
+
+
+
+
+#endif /* STM32F401XE_DRIVER_I2C_H */
+
