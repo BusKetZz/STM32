5c718174e96e5731810a647f062a6e13d2edaa9e
BusKetZz <busketz2k@gmail.com>
2020-06-08 12:34:54 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c: i2c_clock_enable() function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c
@@ -25,3 +25,33 @@
 #define I2C2_CLOCK_DISABLE()    ( RCC->APB1ENR &= ~(1 << 22) )
 #define I2C3_CLOCK_DISABLE()    ( RCC->APB1ENR &= ~(1 << 23) )
 
+
+
+/*****************************************************************************/
+/* I2C RESET MACROS */
+/*****************************************************************************/
+
+#define I2C1_RESET()    do{ (RCC->APB1RSTR |= (1 << 21));\
+    (RCC->APB1RSTR &= ~(1 << 21)); }while(0)
+#define I2C2_RESET()    do{ (RCC->APB1RSTR |= (1 << 22));\
+    (RCC->APB1RSTR &= ~(1 << 22)); }while(0)
+#define I2C3_RESET()    do{ (RCC->APB1RSTR |= (1 << 23));\
+    (RCC->APB1RSTR &= ~(1 << 23)); }while(0)
+
+
+
+/*****************************************************************************/
+/* I2C API DEFINITIONS */
+/*****************************************************************************/
+
+void i2c_clock_enable(i2c_registers_t *i2c_port)
+{
+    if(i2c_port == I2C1) {
+        I2C1_CLOCK_ENABLE();
+    } else if(i2c_port == I2C2) {
+        I2C2_CLOCK_ENABLE();
+    } else if(i2c_port == I2C3) {
+        I2C3_CLOCK_ENABLE();
+    }
+}
+
