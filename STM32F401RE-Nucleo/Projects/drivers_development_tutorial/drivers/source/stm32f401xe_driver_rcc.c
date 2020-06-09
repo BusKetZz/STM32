4a078ea3e6d2cac93286d6dbe35116b09f8c8ab8
BusKetZz <busketz2k@gmail.com>
2020-06-09 15:24:00 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_rcc.c: rcc_get_system_clock_source_speed() function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_rcc.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_rcc.c
@@ -20,3 +20,15 @@ rcc_system_clock_source_t rcc_get_system_clock_source(void)
    return ( (RCC->CFGR >> 2) & 0x3 );
 }
 
+
+
+rcc_system_clock_source_speed_t rcc_get_system_clock_source_speed(
+    rcc_system_clock_source_t clock_source)
+{
+    if(clock_source == rcc_system_clock_source_hsi) {
+        return rcc_system_clock_source_speed_hsi;
+    } else if(clock_source == rcc_system_clock_source_hse) {
+        return rcc_system_clock_source_speed_hse;
+    }
+}
+
