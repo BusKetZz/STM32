0487f7807cf7c4a31a68193f7d32e8c6f9a4bc38
BusKetZz <busketz2k@gmail.com>
2020-06-15 17:04:17 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/: empty general.h header file added

---
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/system/include/general.h
@@ -0,0 +1,17 @@
+/*
+ * Project: Drivers development tutorial
+ * Target MCU: STM32F401XE
+ * Author: Jakub Standarski 
+ *
+ */
+
+#ifndef GENERAL_H
+#define GENERAL_H
+
+
+
+
+
+
+#endif /* GENERAL_H */
+
