27e494bcd97f3c8598988d8fc2a2973716bb801a
BusKetZz <busketz2k@gmail.com>
2020-06-16 12:49:26 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c: i2c_generate_stop_condition() helper function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_i2c.c
@@ -231,6 +231,13 @@ static void i2c_clear_addr_flag(i2c_registers_t *i2c_port)
 
 
 
+static void i2c_generate_stop_condition(i2c_registers_t *i2c_port)
+{
+    i2c_port->CR1 |= (1 << 9);
+}
+
+
+
 static flag_status_t i2c_check_status_register_1(i2c_registers_t *i2c_port,
     i2c_flag_sr1_t flag_name)
 {
