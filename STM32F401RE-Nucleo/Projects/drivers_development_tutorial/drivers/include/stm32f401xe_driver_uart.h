e055171f27f731dc0eb39a876c64ac16769f2899
BusKetZz <busketz2k@gmail.com>
2020-06-27 16:18:10 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/: empty stm32f401xe_driver_uart.h added

---
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/include/stm32f401xe_driver_uart.h
@@ -0,0 +1,20 @@
+/*
+ * Project: Drivers development tutorial
+ * Target MCU: STM32F401XE
+ * Author: Jakub Standarski 
+ *
+ */
+
+#ifndef STM32F401XE_DRIVER_UART_H
+#define STM32F401XE_DRIVER_UART_H
+
+#include "stm32f401xe.h"
+
+
+
+
+
+
+
+#endif
+
