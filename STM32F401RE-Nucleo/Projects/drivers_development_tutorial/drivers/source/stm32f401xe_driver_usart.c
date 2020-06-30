b6e934a9eb6f565be53b07e8e14a4cb2e0905988
BusKetZz <busketz2k@gmail.com>
2020-06-30 15:20:40 +0200

    STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_usart.c: usart_clock_enable() function definition added

--- STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_usart.c
+++ STM32F401RE-Nucleo/Projects/drivers_development_tutorial/drivers/source/stm32f401xe_driver_usart.c
@@ -9,3 +9,22 @@
 
 #include "stm32f401xe.h"
 
+
+
+/*****************************************************************************/
+/* USART API DEFINITIONS */
+/*****************************************************************************/
+
+void usart_clock_enable(usart_registers_t *usart_port)
+{
+    if(usart_port == USART1) {
+        USART1_CLOCK_ENABLE();
+    } else if(usart_port == USART2) {
+        USART2_CLOCK_ENABLE();
+    } else if(usart_port == USART6) {
+        USART6_CLOCK_ENABLE();
+    }
+}
+
+
+
