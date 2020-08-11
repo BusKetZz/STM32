16e16743cf91b4af044bc6f27f8c950da71c5f77
BusKetZz <busketz2k@gmail.com>
2020-08-11 14:01:23 +0200

    stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/rcc.c: function definition "rcc_system_clock_init()" added

--- stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/rcc.c
+++ stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/rcc.c
@@ -17,3 +17,24 @@
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_rcc.h"
 
+
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+void rcc_system_clock_init(void)
+{
+    LL_RCC_HSI_SetCalibTrimming(16);
+
+    LL_RCC_HSI_Enable();
+    while (LL_RCC_HSI_IsReady != 1) {
+        ;
+    }
+
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI) {
+        ;
+    }
+}
+
