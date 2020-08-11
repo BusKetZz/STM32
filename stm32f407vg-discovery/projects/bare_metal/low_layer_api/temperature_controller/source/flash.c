d74aba6f4fb77e63f2e5707a0c6a9688f13771e1
BusKetZz <busketz2k@gmail.com>
2020-08-11 15:17:14 +0200

    stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/flash.c: function definition "flash_set_latency()" added

--- stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/flash.c
+++ stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/source/flash.c
@@ -16,3 +16,18 @@
 
 #include "stm32f4xx_ll_system.h"
 
+
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+void flash_set_latency(void)
+{
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+    while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0) {
+        ;
+    }
+}
+
