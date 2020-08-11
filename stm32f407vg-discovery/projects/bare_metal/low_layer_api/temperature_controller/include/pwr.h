364b1a7df2b2c4cadee42231c9c95492ca1b3a3b
BusKetZz <busketz2k@gmail.com>
2020-08-11 13:18:52 +0200

    stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/include/: "pwr.h" added

---
+++ stm32f407vg-discovery/projects/bare_metal/low_layer_api/temperature_controller/include/pwr.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Jakub Standarski
+ * Date: 11.08.2020
+ *
+ */
+
+
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+void set_regulator_voltage_scalling(void);
+void pwr_clock_enable(void);
+
