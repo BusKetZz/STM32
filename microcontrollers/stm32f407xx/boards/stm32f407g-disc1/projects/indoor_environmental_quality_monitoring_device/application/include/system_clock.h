52b7daf72419093fdaaf591f65855b213bf992b5
BusKetZz <busketz2k@gmail.com>
2020-09-30 00:00:57 +0200

    "system_clock.h" added.
    Path: ./microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/
          indoor_environmental_quality_monitoring_device/application/include/
    
    Changes:
    - "system_clock.h" added and initialized with "system_clock_init()" public
      function prototype.

---
+++ microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/system_clock.h
@@ -0,0 +1,19 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 29.09.2020
+ *
+ */
+
+#ifndef SYSTEM_CLOCK_H
+    #define SYSTEM_CLOCK_H
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+void system_clock_init(void);
+
+#endif /* SYSTEM_CLOCK_H */
+
