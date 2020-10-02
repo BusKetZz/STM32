33ba910ee3a2d8ec4321dc09aca2e91965140ccc
jstand-jakubstandarski <jstand.jakub.standarski@gmail.com>
2020-10-02 14:49:13 +0200

    File "system_clock.h" modified.
    Path: microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/
          indoor_environmental_quality_monitoring_device/application/include/
          system_clock.h
    
    Changes:
    - public defines: "SYSTEM_CLOCK_SOURCE", "SYSTEM_CLOCK_AHB_PRESCALER",
      "SYSTEM_CLOCK_APB1_PRESCALER", "SYSTEM_CLOCK_APB2_PRESCALER" and
      "SYSTEM_CLOCK_HSI_VALUE" added.

--- microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/system_clock.h
+++ microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/system_clock.h
@@ -10,6 +10,27 @@
     #define SYSTEM_CLOCK_H
 
 /*****************************************************************************/
+/* HEADERS */
+/*****************************************************************************/
+
+#include "stm32f4xx_ll_bus.h"
+
+
+
+/*****************************************************************************/
+/* PUBLIC DEFINES */
+/*****************************************************************************/
+
+#define SYSTEM_CLOCK_SOURCE (LL_RCC_SYS_CLKSOURCE_HSI)
+#define SYSTEM_CLOCK_AHB_PRESCALER (LL_RCC_SYSCLK_DIV_1)
+#define SYSTEM_CLOCK_APB1_PRESCALER (LL_RCC_APB1_DIV_1)
+#define SYSTEM_CLOCK_APB2_PRESCALER (LL_RCC_APB2_DIV_1)
+
+#define SYSTEM_CLOCK_HSI_VALUE (uint32_t)16000000
+
+
+
+/*****************************************************************************/
 /* PUBLIC FUNCTIONS PROTOTYPES */
 /*****************************************************************************/
 
