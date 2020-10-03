84ab2d91de88742d35fc8103c9f0ec677ae7791c
jstand-jakubstandarski <jstand.jakub.standarski@gmail.com>
2020-10-03 11:47:49 +0200

    Header file "system_clock.h" modified.
    Path: microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/
          indoor_environmental_quality_monitoring_device/application/include/
          system_clock.h
    
    Changes:
    - redundant defines deleted.

--- microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/system_clock.h
+++ microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/system_clock.h
@@ -21,11 +21,6 @@
 /* PUBLIC DEFINES */
 /*****************************************************************************/
 
-#define SYSTEM_CLOCK_SOURCE    (LL_RCC_SYS_CLKSOURCE_HSI)
-#define SYSTEM_CLOCK_AHB_PRESCALER    (LL_RCC_SYSCLK_DIV_1)
-#define SYSTEM_CLOCK_APB1_PRESCALER    (LL_RCC_APB1_DIV_1)
-#define SYSTEM_CLOCK_APB2_PRESCALER    (LL_RCC_APB2_DIV_1)
-
 #define SYSTEM_CLOCK_HSI_VALUE    (uint32_t)16000000
 
 
