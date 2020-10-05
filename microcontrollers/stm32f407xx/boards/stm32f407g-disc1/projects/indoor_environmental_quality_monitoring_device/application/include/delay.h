bee8fa152b2f9135d1a85f165d05a108a8c3a616
jstand-jakubstandarski <jstand.jakub.standarski@gmail.com>
2020-10-05 22:53:10 +0200

    New header file "delay.h" added.
    Path: microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/
          indoor_environmental_quality_monitoring_device/application/include/
    
    Changes:
    - new header file "delay.h" added;
    - public functions prototypes "delay_us()" and "delay_ms()" added.

---
+++ microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/delay.h
@@ -0,0 +1,22 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 05.10.2020
+ *
+ */
+
+#ifndef DELAY_H
+    #define DELAY_H
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+void delay_us(uint32_t delay_time_us);
+void delay_ms(uint32_t delay_time_ms);
+
+
+
+#endif /* DELAY_H */
+
