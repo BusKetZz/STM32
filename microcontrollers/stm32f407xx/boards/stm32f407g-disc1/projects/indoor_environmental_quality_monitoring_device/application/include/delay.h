801599a3831e433ba5636458659304c1e9026d94
jstand-jakubstandarski <jstand.jakub.standarski@gmail.com>
2020-10-06 13:09:27 +0200

    Header file "delay.h" modified.
    Path: microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/
          indoor_environmental_quality_monitoring_device/application/include/
          delay.h
    
    Changes:
    - public functions prototypes descriptions "delay_timer_init()",
      "delay_us()" and "delay_ms()" added.

--- microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/delay.h
+++ microcontrollers/stm32f407xx/boards/stm32f407g-disc1/projects/indoor_environmental_quality_monitoring_device/application/include/delay.h
@@ -10,11 +10,47 @@
     #define DELAY_H
 
 /*****************************************************************************/
+/* HEADERS */
+/*****************************************************************************/
+
+#include <stdint.h>
+
+
+
+/*****************************************************************************/
 /* PUBLIC FUNCTIONS PROTOTYPES */
 /*****************************************************************************/
 
+/**
+ * @brief   Initialize timer (timer 2) settings, responsible of delay
+ *          functions.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ */
 void delay_timer_init(void);
+
+
+
+/**
+ * @brief   Wait desired amount of microseconds (blocking function).
+ *
+ * @param   delay_time_us - delay time given in microseconds.
+ *
+ * @retval  None.
+ */
 void delay_us(uint32_t delay_time_us);
+
+
+
+/**
+ * @brief   Wait desired amount of milliseconds (blocking function).
+ *
+ * @param   delay_time_ms - delay time given in milliseconds.
+ *
+ * @retval  None.
+ */
 void delay_ms(uint32_t delay_time_ms);
 
 
